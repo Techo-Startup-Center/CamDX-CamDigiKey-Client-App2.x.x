bb5c40f6f26e8df027acfec45c28e8b4bbf0b190
Dara Penhchet <darapenhchet@gmail.com>
2024-09-26 16:13:53 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker pull amazoncorretto:17
+docker pull amazoncorretto:17-alpine-jdk
 ./mvnw clean package jib:dockerBuild -DskipTests
 docker compose -f docker-compose.dev.yaml up -d --force-recreate --build camdigikey_client_app
 docker logs camdigikey_client_app -f --tail 100
